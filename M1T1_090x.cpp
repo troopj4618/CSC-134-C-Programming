d9d2ccc6e398744250c4be38b204216de9dd3d06
Joseph Troop <troopj4618@student.faytechcc.edu>
2025-09-04 14:38:41 +0000

    Finished M1T1

--- M1T1_090x.cpp
+++ M1T1_090x.cpp
@@ -5,3 +5,14 @@ troopj
 9/4/25
 Program will greet the user
 */
+
+#include <iostream>
+using namespace std;
+// say hello to the user
+
+int main() {
+
+cout <<"Hello, user!" <<endl;
+
+    return 0;
+}
