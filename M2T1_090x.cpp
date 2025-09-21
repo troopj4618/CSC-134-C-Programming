e72ae3dca372f2714d7ce7bf2ddbeae008f7a606
Joseph Troop <troopj4618@student.faytechcc.edu>
2025-09-21 15:19:24 +0000

    M2T1 finished

--- M2T1_090x.cpp
+++ M2T1_090x.cpp
@@ -3,13 +3,15 @@
 // 9/21/25
 
 #include <iostream>
+#include <iomanip>
 using namespace std;
 
 int main() {
 
     string first_name, last_name, full_name;
     string product = "potatoes";
-    double cost_each = 1.50;
+    int amount_purchased;
+    double cost_each = 1.99;
     double total_cost;
 
     // greet customer
@@ -20,4 +22,18 @@ int main() {
     cin >> last_name;
     full_name = first_name + " " + last_name;
     cout << "Nice to meet you, " << full_name << endl;
+    // ask how much they want to purchase
+    cout << "How many " << product << " would you like today?";
+    cin >> amount_purchased;
+
+    // calculate total price
+    total_cost = amount_purchased * cost_each;
+    
+    //format all prices to 2 decimals
+    cout << setprecision(2) << fixed;
+    
+    //give the result
+    cout << "For " + amount_purchased << " " << product << endl;
+    cout << "That will be $" << total_cost << endl;
+    cout << "Thank you for shopping with us" << endl;
 }
